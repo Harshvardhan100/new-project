127819abcf68c43f705e158cda19e2af5f713661
Harshvardhan Prajapati <105856460+Harshvardhan100@users.noreply.github.com>
2022-10-17 02:01:14 +0530

    halfdioamond program

---
+++ halfdioanond.java
@@ -0,0 +1,28 @@
+public class halfdioamond {
+    public static void main(String[] args) {
+
+
+
+
+            for (int o = 1; o < 5; o++) {
+
+                for (int p = 0; p < o; p++) {
+                    System.out.print(" ");
+                }
+                for (int s= 4; s > o; s--) {
+                    System.out.print("*");
+                }
+                for(int f=5;f>o;f--){
+                    System.out.print("*");
+                }
+
+              System.out.println("");
+
+            }
+
+
+        }
+
+
+
+}
